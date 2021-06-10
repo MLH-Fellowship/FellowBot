2cf7c2881fe65855a75aeeace83081abafdc10de
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-06-10 09:04:01 +0100

    Change client to bot. Simple bot which takes in a keyword on faq command.

--- bot.py
+++ bot.py
@@ -1,31 +1,20 @@
 # bot.py
 import os
 
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
-# Needed to grab member list.
-# Not in tut, from https://stackoverflow.com/questions/64231025/discord-py-bot-cant-see-members
-intents = discord.Intents.default()
-intents.members = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+@bot.command(name='faq', help='Searches the FAQ for the most relevant section corresponding to the provided keyword.')
+async def dothatthing(ctx, keyword):
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
+    response = keyword
+    await ctx.send(response)
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+bot.run(TOKEN)
 
-client.run(TOKEN)
