3434d9b1aa44bc7ef6a40d86f60a1034d0e835ce
Joe <72690202+joeyouss@users.noreply.github.com>
2021-06-11 04:15:09 +0530

    Update mentalHealthBot.py
    Added the updated code

--- mentalHealthBot.py
+++ mentalHealthBot.py
@@ -1,4 +1,5 @@
 # # bot.py
+# import os
 import json
 import os
 import discord
@@ -10,10 +11,77 @@ import random
 TOKEN = ''
 # enter application id
 GUILD = ''
-flag = False
-client = commands.Bot(command_prefix='.')
 
-# -------------keyword corpus
+# client = commands.Bot(command_prefix='.')
+
+import discord
+from discord.ext import commands
+# from dotenv import load_dotenv
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+
+# ---------------------BOT COMMAND
+bot = commands.Bot(command_prefix='!')
+# ---------------------WEB SCRAPING
+scraping = open("ok.json", encoding="UTF-8")
+scraped = json.load(scraping)
+# ----------------FOR HELPLINE
+helplines = ["Family Violence Prevention Center 1-800-313-1310",
+             "National Sexual Assault Hotline 1-800-656-HOPE (4673)",
+             "Drug Abuse National Helpline 1-800-662-4357",
+             "American Cancer Society 1-800-227-2345",
+             "Eating Disorders Awareness and Prevention 1-800-931-2237",
+             "GriefShare 1-800-395-5755",
+             "Suicide Hotline 1-800-SUICIDE (784-2433)"]
+
+
+#
+@bot.command(name='faq', help='Searches the FAQ for the most relevant section corresponding to the provided keyword.')
+async def faq(ctx, keyword):
+    for i in scraped["intents"]:
+        if(keyword==i["tag"]):
+            quote = i["responses"]
+    # response = quote
+    for i in quote:
+        response = i
+        await ctx.send(response)
+
+
+@bot.command(aliases=['al'])
+async def all(ctx):
+    for i in helplines:
+        res = i
+        await ctx.send(res)
+
+@bot.command(aliases=['depression'])
+async def suicide(ctx):
+    await ctx.send(helplines[6])
+
+@bot.command(aliases=['violenT'])
+async def violence(ctx):
+    await ctx.send(helplines[0])
+
+@bot.command(aliases=['drugs'])
+async def drugabuse(ctx):
+    await ctx.send(helplines[2])
+
+@bot.command(aliases=['assault'])
+async def sexualAssault(ctx):
+    await ctx.send(helplines[1])
+
+@bot.command(aliases=['ILLNESS'])
+async def cancer(ctx):
+    await ctx.send(helplines[3])
+
+@bot.command(aliases=['grie'])
+async def grief(ctx):
+    await ctx.send(helplines[5])
+
+@bot.command(aliases=['eating'])
+async def eatingdisorder(ctx):
+    await ctx.send(helplines[4])
+
+# -------------------------MENTAL WELL BEING BOT
 greetings = ["hi", "hello","how are you", "whats up","hey"]
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "stressed", "hopeless", "unhappy","worthless"]
 problems = ["time management", "busy", "impostor", "coding"]
@@ -21,10 +89,11 @@ suicide = ["kill myself", "kill yourself","suicidal","death"]
 
 # ------------replies
 greet = [
-    "What can I do for you?",
-    "Hi",
+    "Welcome !. What can I do for you?",
+    "Hi!!",
+    "I am so excited to have you here!",
     "Hello there, I am an MLH bot here for your help",
-    "No I am sleeping right now. Just kidding how may I help you?"
+    "No I am sleeping right now. Just kidding !! how may I help you?"
 ]
 
 starter_encouragements = [
@@ -47,16 +116,18 @@ positivereply = [
     "We are extremely sorry to hear that you feel like this , but you are not alone in this. We as MLH put your well being as the top priority. Come talk to us what is bothering you. Help is available, speak with a counselor today by calling the National Suicide Prevention Lifeline at 800-273-8255",
     "WE ARE THERE FOR YOU!!. When you think life isn't worth it, search for some new options. Help is available, speak with a counselor today by calling the National Suicide Prevention Lifeline at 800-273-8255"
 ]
-@client.event
+
+# -------------------------TRIGGERING BOT EVENT
+@bot.event
 async def on_ready():
     print('Bot is ready.')
-
+#-------------------------RANDOM QUOTE SUGGESTION
 def get_qoute():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
     return (quote)
-
+# ------------------------GET RANDOM JOKES
 def joke():
     url = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Christmas?blacklistFlags=nsfw,political,racist,sexist,explicit&type=twopart"
     response = requests.get(url)
@@ -65,16 +136,17 @@ def joke():
     jokes = json_data["setup"] + "  -  " + json_data["delivery"]
     return(jokes)
 
-
-@client.event
+# -----------------------MAIN BOT CHAT EVENT
+@bot.event
 async def on_message(message):
     flag = False
-    await client.process_commands(message)
+    gify = False
+    await bot.process_commands(message)
     msg = message.content
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     if any(word in msg for word in greetings):
-        # flag = True
+        gify = True
         response = random.choice(greet)
         await message.channel.send(response)
 
@@ -99,8 +171,10 @@ async def on_message(message):
         r2 = joke()
         await message.channel.send(r2)
 
-    if 'quote' in message.content.lower():
+    if 'quote' in msg.lower():
         response = get_qoute()
         await message.channel.send(response)
 
-client.run(TOKEN)
+# ---------------------INITIALIZING THE BOT
+bot.run(TOKEN)
+
