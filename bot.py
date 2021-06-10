db6350aa0c350fe20a4b2567c929c41868804ba7
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-06-10 09:16:08 +0100

    Renamed dothatthing to faq for use in scraping.

--- bot.py
+++ bot.py
@@ -11,10 +11,15 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='faq', help='Searches the FAQ for the most relevant section corresponding to the provided keyword.')
-async def dothatthing(ctx, keyword):
+async def faq(ctx, keyword):
 
     response = keyword
     await ctx.send(response)
 
 bot.run(TOKEN)
 
+'''
+@bot.command(name='dailyreminder' help='Messages with daily reminders.')
+async def dailyreminder(ctx, time, int:minsbefore, int:gmtplus):
+    import time
+'''
