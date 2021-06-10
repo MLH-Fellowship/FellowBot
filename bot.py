645c7e3d40187e3f00bab728b5cfbee43a6107c7
George A. McCarthy <65653499+georgeamccarthy@users.noreply.github.com>
2021-06-10 08:32:01 +0100

    Grab server member list and print.

--- bot.py
+++ bot.py
@@ -6,11 +6,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+# Needed to grab member list.
+# Not in tut, from https://stackoverflow.com/questions/64231025/discord-py-bot-cant-see-members
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
